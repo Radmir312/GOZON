2eddb6b0fa745a19c8303107f86190e4395a7133
FORL <FORL@WIN11WM>
2025-12-19 15:34:30 +0500

    Добавлено БД без ничего, слегка работает авторизация

---
+++ Database.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data.SQLite;
+using System.IO;
+
+namespace GOZON
+{
+    public static class Database
+    {
+        public static string DbPath = Path.Combine(
+        Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.FullName,
+        "warehouse.db"
+        );
+
+        public static SQLiteConnection Open()
+        {
+            var conn = new SQLiteConnection($"Data Source={DbPath};Version=3;");
+            conn.Open();
+            return conn;
+        }
+    }
+}
