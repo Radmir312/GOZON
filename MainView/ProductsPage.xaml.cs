d5d227d8ce1c51b9a79f9d3ba54704d1e7e13b33
klg <klg@111-STD-18>
2025-12-22 12:47:19 +0500

    Add Products

--- MainView/ProductsPage.xaml.cs
+++ MainView/ProductsPage.xaml.cs
@@ -1,33 +1,95 @@
-﻿using System;
+﻿using GOZON.Models;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SQLite;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace GOZON.MainView
 {
-    /// <summary>
-    /// Interaction logic for ProductsPage.xaml
-    /// </summary>
     public partial class ProductsPage : Page
-    {
+    {     
         public ProductsPage()
         {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            Manager.MainFrame.GoBack();
+            LoadProducts();
+        }
+
+        private void LoadProducts()
+        {
+            try
+            {
+                var products = new List<Product>();
+
+                using (var conn = Database.Open())
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT 
+                            p.Id,
+                            p.Name,
+                            p.SKU,
+                            p.Price,
+                            COALESCE(SUM(s.Quantity), 0) as TotalQuantity
+                        FROM Products p
+                        LEFT JOIN Stock s ON p.Id = s.ProductId
+                        GROUP BY p.Id
+                        ORDER BY p.Name";
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            products.Add(new Product
+                            {
+                                Id = reader.GetInt32(0),
+                                Name = reader.GetString(1),
+                                SKU = reader.IsDBNull(2) ? "" : reader.GetString(2),
+                                Price = Convert.ToDecimal(reader.GetDouble(3)),
+                                TotalQuantity = reader.GetInt32(4)
+                            });
+                        }
+                    }
+                }
+
+                ProductsGrid.ItemsSource = products;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка загрузки: " + ex.Message);
+            }
+        }
+
+        private void Refresh_Click(object sender, RoutedEventArgs e)
+        {
+            LoadProducts();
+        }
+
+        private void AddProduct_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (var conn = Database.Open())
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        INSERT INTO Products (Name, SKU, Price, MinQuantity)
+                        VALUES ('Новый товар', '', 0, 0)";
+
+                    cmd.ExecuteNonQuery();
+
+                    MessageBox.Show("Товар добавлен");
+                    LoadProducts();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message);
+            }
         }
     }
 }
---
+++ Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace GOZON.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public int Login { get; set; }
+        public int PasswordHash { get; set; }
+        public int FullName { get; set; }
+        public int Email { get; set; }
+        public int Role { get; set; }
+        public int CreatedAt { get; set; }
+    }
+}
